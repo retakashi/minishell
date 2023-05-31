31d152cad1146df06b364146485ea82052802f85
Shahrukh Raza <shahrukh4062@gmail.com>
2023-05-31 11:52:52 +0900

    makefile is ok

--- Makefile
+++ Makefile
@@ -6,17 +6,15 @@
 #    By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/30 10:05:15 by razasharuku       #+#    #+#              #
-#    Updated: 2023/05/30 10:05:54 by razasharuku      ###   ########.fr        #
+#    Updated: 2023/05/31 11:51:08 by razasharuku      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 
-
 CC = cc
 NAME = $(NAME)
-NAME	=	minishell
-CFLAGS	=	-Wall -Wextra -Werror
-INC		=	-I./libft/ 
+NAME	 =	minishell
+CFLAGS	 =	-Wall -Werror -Wextra 
 
 SRCS	=	main.c
 
@@ -28,7 +26,7 @@ LIBFT		=	./libft/libft.a
 
 $(NAME):	$(OBJS)
 	$(MAKE) -C ./libft
-	$(CC) $(CFLAGS) $(LIBFT) $(SRCS) -o $(NAME)
+	$(CC) $(CFLAGS) $(LIBFT) $(SRCS) -o $(NAME) -lreadline
 
 clean:
 	$(MAKE) -C ./libft clean
--- main.c
+++ main.c
@@ -6,7 +6,33 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 10:02:28 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/05/30 10:02:29 by razasharuku      ###   ########.fr       */
+/*   Updated: 2023/05/31 11:09:11 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int main()
+{
+    char *line = NULL;
+
+    while (1)
+    {
+        line = readline("> ");
+        if (line == NULL || strlen(line) == 0)
+        {
+            free(line);
+            break;
+        }
+        printf("line is '%s'\n", line);
+        add_history(line);
+        free(line);
+    }
+    printf("exit\n");
+    return 0;
+}
