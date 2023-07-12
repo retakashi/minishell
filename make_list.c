effa5a04902348576876a7dca5cb4b7ab1b9d571
Shahrukh Raza <shahrukh4062@gmail.com>
2023-07-12 15:33:03 +0900

    lexer is 50% done

--- make_list.c
+++ make_list.c
@@ -6,7 +6,7 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 17:11:24 by sraza             #+#    #+#             */
-/*   Updated: 2023/07/12 15:13:28 by razasharuku      ###   ########.fr       */
+/*   Updated: 2023/07/12 15:29:12 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,6 +87,7 @@ t_word_list *copy_make_list(char *line)
 	t_word_list	*tmp;
 
 	i = 0;
+	//make first_list
 	while (line[i] != ' ' && line[i] != '\t' && line[i] != '\0')
 		i++;
 	string = creat_list(line, i);
@@ -95,6 +96,7 @@ t_word_list *copy_make_list(char *line)
 		line++;
 	tmp = string;
 	i = 0;
+	//make intermediate_list
 	while (*line != '\0' && line[i] != '\0')
 	{
 		if (line[i] == ' ' || line[i] == '\t')
@@ -105,6 +107,14 @@ t_word_list *copy_make_list(char *line)
 			i = -1;
 		}
 		i++;
+	}
+	//make last_list
+	while (line[i] != ' ' && line[i] != '\t' && line[i] != '\0')
+		i++;
+	if (i != 0)
+	{
+		string->next = creat_list(line, i);
+		string->next->next = NULL;
 	}
 	return (tmp);
 }
