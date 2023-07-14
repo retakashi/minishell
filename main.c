464324905883d6e87e00dfc560469516ca7d5033
retakashi <119090016+retakashi@users.noreply.github.com>
2023-07-15 00:14:22 +0900

    update is_command

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: reira <reira@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 10:02:28 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/07/14 17:08:12 by reira            ###   ########.fr       */
+/*   Updated: 2023/07/14 23:48:47 by reira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,70 @@
 #include "minishell.h"
 //meta_char | & ; ( ) space tab  < > == less,great,| == pipe_char
 
-void	read_word_list(t_word_list *word_list, t_env_list *env_list,t_fd fd_struct)
+void	read_word_list(t_word_list *word_list, t_env_list *env_list,
+		t_fd fd_struct)
 {
 	while (word_list != NULL)
 	{
 		if (word_list != NULL && word_list->flag == less_less)
-			create_heredoc(&word_list,&fd_struct);
+			create_heredoc(&word_list, &fd_struct);
 		else if (word_list != NULL && (word_list->flag == great
 					|| word_list->flag == less))
-			main_redirection(&word_list,&fd_struct);
+			main_redirection(&word_list, &fd_struct);
 		else if (word_list != NULL && word_list->flag == great_great)
-			main_additionally_write(&word_list,&fd_struct);
+			main_additionally_write(&word_list, &fd_struct);
 		else if (word_list != NULL && word_list->flag == command
 				&& is_builtin(word_list))
-			main_builtin(&word_list,&env_list);
+			main_builtin(&word_list, &env_list);
 		else if (word_list != NULL && word_list->flag == command
 				&& !is_builtin(word_list))
 			main_command(&word_list);
 	}
 }
 
-void	is_command_list_head(t_word_list **head)
+bool	is_inequality_sign(int flag)
 {
-	if ((*head)->flag == arguments)
-		(*head)->flag = command;
-	else if ((*head)->flag == great || (*head)->flag == great
-			|| (*head)->flag == less || (*head)->flag == less_less)
-	{
-		if ((*head)->next == NULL)
-			perror_exit("newline", SYNTAX_ERROR);
-		*head = (*head)->next;
-		if ((*head)->flag == pipe_char)
-			perror_exit("|", SYNTAX_ERROR);
-		if ((*head)->next != NULL)
-		{
-			*head = (*head)->next;
-			if ((*head)->flag == arguments)
-				(*head)->flag = command;
-		}
-	}
-	else
-		perror_exit((*head)->word, SYNTAX_ERROR);
+	if (flag == great || flag == great || flag == less || flag == less_less)
+		return (true);
+	return (false);
 }
 
 void	is_command(t_word_list **head)
 {
-	is_command_list_head(head);
+	if (*head == NULL)
+		return ;
+	if ((*head)->flag == arguments)
+		(*head)->flag = command;
+	else if (is_inequality_sign((*head)->flag == true))
+	{
+		while ((*head != NULL && (*head)->next != NULL)
+			&& (is_inequality_sign((*head)->flag) == true
+				&& (*head)->next->flag == arguments))
+			*head = (*head)->next->next;
+		if (is_inequality_sign((*head)->flag) == true && (*head)->next == NULL)
+			perror_exit("newline", SYNTAX_ERROR);
+		if (is_inequality_sign((*head)->flag) == true
+			&& (*head)->next->flag != arguments)
+			perror_exit((*head)->next->word, SYNTAX_ERROR);
+		*head = (*head)->next;
+		(*head)->flag = command;
+	}
+}
+
+void	traverse_list_until_pipe(t_word_list **head)
+{
+	is_command(head);
 	while (*head != NULL)
 	{
 		*head = (*head)->next;
 		while (*head != NULL && (*head)->flag != pipe_char)
 			*head = (*head)->next;
-		if (*head == NULL || (*head)->next == NULL)
+		if (*head == NULL)
 			break ;
-		if ((*head)->flag == pipe_char)
+		else if ((*head)->flag == pipe_char)
 		{
 			*head = (*head)->next;
-			(*head)->flag == command;
+			is_command(head);
 		}
 	}
 	*head = g_shell_struct->word_head;
@@ -101,7 +108,7 @@ void	init_minishell(char **envp)
 	if (envp != NULL)
 		get_envp_list(envp, &env_list_head);
 	init_fd_struct(&fd_struct);
-	read_word_list(word_head, env_list_head,fd_struct);
+	read_word_list(word_head, env_list_head, fd_struct);
 }
 
 // __attribute__((destructor))
