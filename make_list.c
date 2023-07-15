3a6d86a961eeb7fd23fb0f8531a4104f053df5f1
Shahrukh Raza <shahrukh4062@gmail.com>
2023-07-15 21:33:47 +0900

    almost splitted

--- make_list.c
+++ make_list.c
@@ -3,39 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   make_list.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
+/*   By: sraza <sraza@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 17:11:24 by sraza             #+#    #+#             */
-/*   Updated: 2023/07/15 15:40:35 by razasharuku      ###   ########.fr       */
+/*   Updated: 2023/07/15 21:13:51 by sraza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minishell.h"
-
 
 int	is_meta(char *str)
 {
 	t_flags	meta_num;
 
 	meta_num = 0;
-	if (ft_strrchr(str, '|'))
+	if (ft_strnstr(str, "|", ft_strlen(str)))
 		meta_num = pipe_char;
-	if (ft_strncmp(str, ">", ft_strlen(str)))
+	if (ft_strnstr(str, ">", ft_strlen(str)))
 		meta_num = great;
-	// if (ft_strncmp(str, ">>", ft_strlen(str)))
-	// 	meta_num = great_great;
-	if (ft_strncmp(str, "<", ft_strlen(str)))
+	if (ft_strnstr(str, ">>", ft_strlen(str)))
+		meta_num = great_great;
+	if (ft_strnstr(str, "<", ft_strlen(str)))
 		meta_num = less;
-	// if (ft_strncmp(str, "<<", ft_strlen(str)))
-	// 	meta_num = less_less;
+	if (ft_strnstr(str, "<<", ft_strlen(str)))
+		meta_num = less_less;
 	return (meta_num);
 }
 
 t_word_list	*split_list(t_word_list *string, char *flag)
 {
 	t_word_list *tmp;
+	t_word_list *tmp2;
 	t_word_list *new;
-
 	char 		**split;
 	int			i;
 
@@ -44,6 +43,7 @@ t_word_list	*split_list(t_word_list *string, char *flag)
 	split = split_str(string->word, flag);
 	if (split == NULL)
 		return (tmp);
+	tmp2 = string->next;
 	string->word = ft_strdup(split[0]);
 	new = ft_newlst(flag);
 	string->next = new;
@@ -53,11 +53,15 @@ t_word_list	*split_list(t_word_list *string, char *flag)
 		new = creat_list(split[i], ft_strlen(split[i]));
 		string->next = new;
 		string = string->next;
-		new = ft_newlst(flag);
-		string->next = new;
-		string = string->next;
+		if (split[i + 1] != NULL)
+		{
+			new = ft_newlst(flag);
+			string->next = new;
+			string = string->next;
+		}
 		i++;
 	}
+	string->next = tmp2;
 	printf("%s come here!! \n", flag);
 	return (tmp);
 }
@@ -73,12 +77,14 @@ t_word_list	*find_meta(t_word_list *string)
 		flag = is_meta(string->word);
 		if (flag == 5 && (ft_strlen(string->word) > 1))
 			string = split_list(string, "|");
-		else if (flag == 6 && (ft_strlen(string->word) > 1))
+		if (flag == 6 && (ft_strlen(string->word) > 1))
 			string = split_list(string, ">");
-		else if (flag == 7 && (ft_strlen(string->word) > 2))
+		if (flag == 7 && (ft_strlen(string->word) > 2))
 			string = split_list(string, ">>");
-		else if (flag == 8 && (ft_strlen(string->word) > 1))
+		if (flag == 8 && (ft_strlen(string->word) > 1))
 			string = split_list(string, "<");
+		if (flag == 9 && (ft_strlen(string->word) > 2))
+			string = split_list(string, "<<");
 		string = string->next;
 	}
 	return (tmp);
