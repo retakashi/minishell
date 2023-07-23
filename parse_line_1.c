86a3210dce4cdcba04e6c044fdc41132adf8dc94
Shahrukh Raza <shahrukh4062@gmail.com>
2023-07-23 18:09:17 +0900

    raza

--- parse_line_1.c
+++ parse_line_1.c
@@ -6,7 +6,7 @@
 /*   By: sraza <sraza@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 10:44:21 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/07/23 17:47:10 by sraza            ###   ########.fr       */
+/*   Updated: 2023/07/23 18:08:26 by sraza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,38 @@ t_word_list	*find_syntax_er(t_word_list	*string)
 
 t_word_list	*redirect_command(t_word_list *tmp)
 {
-	tmp->next->flag = tmp->flag + 5;
+	if (tmp->next != NULL)
+		tmp->next->flag = tmp->flag + 5;
 	if (tmp->next != NULL && tmp->next->next != NULL)
 	{
 		tmp = tmp->next->next;
 		tmp->flag = 1;
 	}
+	return (tmp);
+}
+
+t_word_list	*set_flags_per_list(t_word_list	*tmp)
+{
+	if (tmp->flag == 5 && tmp->next != NULL)
+	{
+		if (is_meta(tmp->next->word) > 5 && is_meta(tmp->next->word) < 10)
+		{
+			tmp = tmp->next;
+			tmp->flag = is_meta(tmp->word);
+			tmp = redirect_command(tmp);
+		}
+		else
+			tmp->next->flag = 1;
+	}
+	if (tmp->flag == 1 && tmp->next != NULL && tmp->next->word[0] == '-')
+		tmp->next->flag = 2;
+	if (tmp->flag == 1 && tmp->next != NULL && tmp->next->word[0] != '-' &&
+		!(is_meta(tmp->next->word) > 3 && is_meta(tmp->next->word) < 15))
+		tmp->next->flag = 3;
+	if (tmp->flag > 5 && tmp->flag < 10 && tmp->next != NULL)
+		tmp->next->flag = tmp->flag + 5;
+	if (tmp->flag == 0)
+		tmp->flag = 3;
 	return (tmp);
 }
 
@@ -50,26 +76,27 @@ t_word_list	*set_flags(t_word_list	*string)
 			if (is_meta(tmp->word) != 0)
 				tmp->flag = is_meta(tmp->word);
 		}
-		if (tmp->flag == 5 && tmp->next != NULL)
-		{
-			if (is_meta(tmp->next->word) > 5 && is_meta(tmp->next->word) < 10)
-			{
-				tmp = tmp->next;
-				tmp->flag = is_meta(tmp->word);
-				tmp = redirect_command(tmp);
-			}
-			else
-				tmp->next->flag = 1;
-		}
-		if (tmp->flag == 1 && tmp->next->word[0] == '-' && tmp->next != NULL)
-			tmp->next->flag = 2;
-		if (tmp->flag == 1 && tmp->next->word[0] != '-' &&
-			!(is_meta(tmp->next->word) > 3 && is_meta(tmp->next->word) < 15) && tmp->next != NULL)
-			tmp->next->flag = 3;
-		if (tmp->flag > 5 && tmp->flag < 10 && tmp->next != NULL)
-			tmp->next->flag = tmp->flag + 5;
-		if (tmp->flag == 0)
-			tmp->flag = 3;
+		tmp = set_flags_per_list(tmp);
+		// if (tmp->flag == 5 && tmp->next != NULL)
+		// {
+		// 	if (is_meta(tmp->next->word) > 5 && is_meta(tmp->next->word) < 10)
+		// 	{
+		// 		tmp = tmp->next;
+		// 		tmp->flag = is_meta(tmp->word);
+		// 		tmp = redirect_command(tmp);
+		// 	}
+		// 	else
+		// 		tmp->next->flag = 1;
+		// }
+		// if (tmp->flag == 1 && tmp->next != NULL && tmp->next->word[0] == '-')
+		// 	tmp->next->flag = 2;
+		// if (tmp->flag == 1 && tmp->next != NULL && tmp->next->word[0] != '-' &&
+		// 	!(is_meta(tmp->next->word) > 3 && is_meta(tmp->next->word) < 15))
+		// 	tmp->next->flag = 3;
+		// if (tmp->flag > 5 && tmp->flag < 10 && tmp->next != NULL)
+		// 	tmp->next->flag = tmp->flag + 5;
+		// if (tmp->flag == 0)
+		// 	tmp->flag = 3;
 		tmp = tmp->next;
 	}
 	return (string);
