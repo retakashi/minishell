039d2e644a54e2bd751531dbe0636efa1f17938f
Shahrukh Raza <shahrukh4062@gmail.com>
2023-08-13 13:24:49 +0900

    ok

--- parse/parse_line_1.c
+++ parse/parse_line_1.c
@@ -6,7 +6,7 @@
 /*   By: sraza <sraza@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 13:39:25 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/08/11 22:58:41 by sraza            ###   ########.fr       */
+/*   Updated: 2023/08/13 13:24:20 by sraza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,18 @@ static	int	check_sp(char *line)
 	return (0);
 }
 
+// static	t_word_list	*trim_quotes(t_word_list *string)
+// {
+// 	t_word_list	*tmp;
+
+// 	tmp = string;
+// 	while (string)
+// 	{
+// 		if (string->flag == 3)
+// 			ft_strtrim();
+// 	}
+// }
+
 t_word_list	*parse_line(char *line)
 {
 	t_word_list	*string;
@@ -41,6 +53,7 @@ t_word_list	*parse_line(char *line)
 	string = set_flags(string);
 	string = tmp;
 	print_words(string);
+	// string = trim_quotes(string);
 	return (tmp);
 }
 
