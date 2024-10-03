2643acb5154514503cf868b40105cfb25b8b325b
retakashi <119090016+retakashi@users.noreply.github.com>
2024-10-03 21:29:39 +0900

    delete: 余計なファイル

--- objs/get_next_line.d
+++
@@ -1,6 +0,0 @@
-objs/get_next_line.o: gnl/get_next_line.c gnl/../libft/libft.h \
-  gnl/get_next_line.h
-
-gnl/../libft/libft.h:
-
-gnl/get_next_line.h:
--- objs/get_next_line_utils.d
+++
@@ -1,6 +0,0 @@
-objs/get_next_line_utils.o: gnl/get_next_line_utils.c \
-  gnl/../libft/libft.h gnl/get_next_line.h
-
-gnl/../libft/libft.h:
-
-gnl/get_next_line.h:
